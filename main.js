c341eeb6be09b0ec14803034f5c8c7c2da4c3723
Gust <gustdevo@gmail.com>
2026-09-06 10:50:52 -0300

    Añadida la herramienta de borrado

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ tamanoPincel = document.getElementById("tamanoPincel");
 color = document.getElementById("color");
 
 
-contexto.strokeStyle="rgba(128,128,128,0.5)";
-contexto.fillStyle="rgba(128,128,128,0.5)";
+contexto.strokeStyle="rgba(128,128,128,1.0)";
+contexto.fillStyle="rgba(128,128,128, 1.0)";
 color.addEventListener("change", (e) => {
 	contexto.strokeStyle= e.target.value;
 	contexto.fillStyle= e.target.value;
@@ -17,20 +17,19 @@ tamanoPincel.addEventListener("change", (e) => {
 });
 
 canvas.addEventListener('click',function(e){ //ante un simple click, creo un punto
-	contexto.beginPath();
-	contexto.arc(e.offsetX, e.offsetY, contexto.lineWidth, 0, 2 * Math.PI);
-	contexto.fill();
+	if (getRadioValue("tool") === "pincel") {
+		contexto.globalCompositeOperation = 'source-over';
+	} else if(getRadioValue("tool") === "borrador") {
+		contexto.globalCompositeOperation = 'destination-out';
+	}
+		contexto.beginPath();
+		contexto.arc(e.offsetX, e.offsetY, contexto.lineWidth, 0, lineWidth);
+		contexto.fill();
 }); //contemplo tambiÃ©n trazos continuos sin soltar el botÃ³n
 
 canvas.addEventListener('mousemove',function(e){ //al mover el puntero sobre el lienzo
-if(e.buttons) contexto.arc(e.offsetX,e.offsetY,contexto.lineWidth, 0, 2 * Math.PI); //si el botÃ³n estÃ¡ presionado
+if(e.buttons) contexto.fillRect(e.offsetX,e.offsetY, 0, 0); //si el botÃ³n estÃ¡ presionado
 });
-
-canvas.addEventListener('click',function(e){ //ante un simple click, creo un punto
-	contexto.beginPath();
-	contexto.arc(e.offsetX, e.offsetY, contexto.lineWidth, 0, 2 * Math.PI);
-	contexto.fill();
-}); //contemplo tambiÃ©n trazos continuos sin soltar el botÃ³n
 
 ahora=false; //flag que indica si el botÃ³n izquierdo estÃ¡ presionado en el evento actual
 
@@ -39,7 +38,8 @@ canvas.addEventListener('mouseup',function(e){ahora=false;}); //soltaron el bot�
 
 antes=false; //flag que indica si estaba presionado en el evento anterior
 
-function inicioTrazo(e){ if(ahora){ //si estÃ¡ presionado, comienza a dibujar una linea continua
+function inicioTrazo(e){ if(ahora){//si estÃ¡ presionado, comienza a dibujar una linea continua
+contexto.lineJoin = contexto.lineCap = 'round';
 contexto.beginPath(); //inicio el trazado
 contexto.moveTo(e.offsetX,e.offsetY); //en la posiciÃ³n de inicio
 }
@@ -57,3 +57,16 @@ antes=false; //el flag del evento anterior, toma el valor de botÃ³n suelto del
 canvas.addEventListener('mouseenter',function(e){ //el curso entrÃ³ al lienzo
 if(!e.buttons) ahora=false; else inicioTrazo(e); //si el botÃ³n no estÃ¡ presionado, no dibujo
 });
+
+function getRadioValue(herramientas)
+{
+    var elements = document.getElementsByName(herramientas);
+    for (var i = 0, l = elements.length; i < l; i++)
+    {
+        if (elements[i].checked)
+        {
+            return elements[i].value;
+        }
+    }
+}
+
